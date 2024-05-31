ff5a80e067429196ae4de110335d920b1d7a3dba
Yuken Subedi <yukensubedi@gmail.com>
2024-05-31 09:33:32 +0545

    Blog generation

--- filemanager/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns = [ 
-    path('url/<int:id>/', views.image_url, name='url'),
-    path('gallery/', views.ImageListView.as_view(), name='gallery'),
-    path('get_images/', views.get_images, name='get_images'),
-    path('upload_image/', views.upload_image, name='upload_image'),
-]
--- userprofile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path, include
-from account.urls import urlpatterns as accountUrls
-from . import views 
-
-urlpatterns = [
-    re_path(r'^login/', views.LoginView.as_view(), name='user_login'),
-    re_path(r'^signup/', views.SignupView.as_view(), name='user_signup'),
-]+ accountUrls
