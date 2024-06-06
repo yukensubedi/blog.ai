62ad04c36d23d03f5ef6043c8f3fff5ea4964c03
Yuken Subedi <yukensubedi@gmail.com>
2024-06-06 22:01:21 +0545

    Bug Fix

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf.urls.static import static
 
 from django.conf import settings
 
@@ -28,6 +29,8 @@ urlpatterns = [
     path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
 ]
 
+
+
 if settings.DEBUG:
-    from django.conf.urls.static import static
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
