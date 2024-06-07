5de1da5a042e5cd43bd203d1010ab3547358615a
Yuken Subedi <51102209+yukensubedi@users.noreply.github.com>
2024-06-07 08:27:50 +0545

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -159,16 +159,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
 
-if DEBUG:
-    import mimetypes
-    mimetypes.add_type("application/javascript", ".js", True)
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
